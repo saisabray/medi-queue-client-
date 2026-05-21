ca790d307d1b82b6fe95b02fe81939e5f272a29f
saisabray <saisabray1402@gmail.com>
2026-05-21 20:26:43 +0600

    test

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,51 +1,31 @@
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
-import { jwt } from "better-auth/plugins";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { jwt } from "better-auth/plugins";
 
-const uri = process.env.MONGODB_URI;
-
-if (!uri) {
-  throw new Error("MONGODB_URI is missing");
-}
-
-const globalForMongo = globalThis;
-
-let client;
-let clientPromise;
-
-if (!globalForMongo._mongoClientPromise) {
-  client = new MongoClient(uri);
-  globalForMongo._mongoClientPromise = client.connect();
-}
-
-clientPromise = globalForMongo._mongoClientPromise;
-
-const mongoClient = await clientPromise;
-const db = mongoClient.db("test-2");
+const client = new MongoClient(process.env.MONGODB_URI);
+const db = client.db("test-2");
 
 export const auth = betterAuth({
-  database: mongodbAdapter(db, { client: mongoClient }),
-
+  database: mongodbAdapter(db, {
+    client,
+  }),
   emailAndPassword: {
     enabled: true,
   },
-
   socialProviders: {
     google: {
-      enabled: true,
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     },
   },
-
   session: {
     cookieCache: {
       enabled: true,
       strategy: "jwt",
-      maxAge: 60 * 60 * 24 * 7,
+
+      maxAge: 7 * 24 * 60 * 60,
     },
   },
-
   plugins: [jwt()],
 });
